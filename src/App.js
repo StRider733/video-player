aab4409cb31a06f8d5c08d4b50c020f34cb7f821
StRider733 <118477683+StRider733@users.noreply.github.com>
2023-03-27 00:46:24 +0300

    css fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import ReactPlayer from "react-player/youtube"
 
 function App() {
+
+  const vidUrl = "https://youtu.be/i7MLT4MmAK8"
+
+
   return (
     <div className="App">
-      <h1>react video player</h1>
+      <h1>react video player example</h1>
+      <ReactPlayer className="videoPlayer"
+        url={vidUrl}//video url getting from vidUrl component.
+        playing={false}//not otomaticly playin.
+        volume={0.5}// volume is mid.
+      />
     </div>
   );
 }
